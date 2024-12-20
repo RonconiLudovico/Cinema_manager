892c35b16fdde112763293420afe64f6dc714d6e
RonconiLudovico <149304890+RonconiLudovico@users.noreply.github.com>
2024-12-20 18:11:44 +0100

    Update decision_making.py

--- finalProject/src/utils/decision_making.py
+++ finalProject/src/utils/decision_making.py
@@ -2,22 +2,23 @@
 # I created a function in which depending on the input passed as arguments will run the relative function
 
 from utils.view_infos import show_infos
+from utils.reserve_seat import choose_movie
 
 menus = {
     "0" : "Make Reservation [1] ---- Check Availability [2] ---- Manage Reservation [3] ---- Exit [9]\n",
-    "1" : "Movie 1 [1] ---- Movie 2 [2] ---- Movie 3 [3] ---- Movie 4 [4] ---- Movie 5 [5] ---- Go Back [0]\n",
+    "1" : "\nGo Back [0]\n",
     "2" : "\nGo Back [0]\n",
     "3" : "Add a seat [1] ---- Move a seat [2] ---- Remove a seat [3] ---- Go Back [0]\n",
     "9" : "Thank you for using our Cinema booking Manager :)"
     }
 
 actions = {
-    "2" : show_infos()
+    "1" : choose_movie,
+    "2" : show_infos
 }
 
 def navigate(choice):
     if choice in actions:
-        print(actions[choice])
-    return menus[choice]
-
-
+        actions[choice]()
+    if choice in menus:
+        print(menus[choice])
